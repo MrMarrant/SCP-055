077ea48c05309a20d074c431160513975172610d
MrMarant <laughymclaugher@gmail.com>
2024-06-07 00:13:31 +0200

    [FEATURE] Add chinese lang

---
+++ lua/scp_055/language/sh_zh-CN.lua
@@ -0,0 +1,84 @@
+-- SCP-055, A representation of a paranormal object on a fictional series on the game Garry's Mod.
+-- Copyright (C) 2023  MrMarrant aka BIBI.
+
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+local LANG_zh_CN = {
+    enter = "确认",    
+    quit = "退出",    
+    not055 = "它不是你",    
+    notstupid = "它不笨",    
+    notalone = "它不孤独",    
+    isalive = "它不是活的",   
+    introtalkevent_1 = "我听到声音,声音如雷",    
+    introtalkevent_2 = "听见四活物中的一个活物,说:你来!",    
+    introtalkevent_3 = "我就观看",    
+    introtalkevent_4 = "见有一匹白马",    
+    outrotalkevent_1 = "我听见中间的活物说:你来!",    
+    outrotalkevent_2 = "我就观看，见有一匹灰马",    
+    outrotalkevent_3 = "骑在马上的名字叫死",    
+    outrotalkevent_4 = "地狱随之而来",    
+    ohsothatis = "哦 ... 原来是这样的",    
+    needcard_description = "如果想需要安全卡来打开SCP-055的箱子,请启用",    
+    radiuseffect_description = "公文包的半径效果,设置为0禁用它",    
+    warningsettings = "只有超级管理员可以改变这些数值,其他所有人员的更改均不生效.",    
+    useonceperlife_description = "如果选中,玩家一生中只能使用SCP-055一次.",    
+    maxdurationgameevent_description = "在迷你游戏事件中,设置tp进入最后一屏的最大时间(秒).",
+    subtiles_1 = "我能进来吗?",    
+    subtiles_2 = "当然",    
+    subtiles_3 = "这里的碎玻璃足够给所有人用了",    
+    subtiles_4 = "我在开玩笑,你知道的",    
+    subtiles_5 = "你可以去找张椅子.",    
+    subtiles_6 = "没事,这套装挺结实的",    
+    subtiles_7 = "那随你便了.",    
+    subtiles_8 = "你弄到的这套装备真不错啊.",    
+    subtiles_9 = "想交换一下吗",    
+    subtiles_10 = "想都别想!我读过文档的",    
+    subtiles_11 = "值得一试啊.",    
+    subtiles_12 = "你好久没笑过了,哈?",    
+    subtiles_13 = "没什么东西可以去笑的.",    
+    subtiles_14 = "电视上全是骚扰机器人的时候也没笑过?",    
+    subtiles_15 = "好吧,那确实有点好笑",    
+    subtiles_16 = "所以,你也逃出来了.我是说,我猜你是基金会的人,而不是我这辈子在这得罪的众人中来报复的某一个.",    
+    subtiles_17 = "那不是同一种人吗?",    
+    subtiles_18 = "现在你明白了",    
+    subtiles_19 = "对,我是基金会的.我是说曾是基金会的.很幸运在这一切开始之前进到了这套装里然后逃出来了.你呢?",    
+    subtiles_20 = "好吧,我是高级职员——我们还比别人更早地被告知这个计划,但是我靠我居然记不得是什么计划了.可能是因为那第二份文件.",    
+    subtiles_21 = "第二份文件?你看到了?里面是什么?!",    
+    subtiles_22 = "喔,冷静点,孩子,我们有的是时间.那就是一大堆图像而已——鸡蛋,树木,宗教的东西",    
+    subtiles_23 = "本身对我来说什么都不是，但是我猜它们之内有某种编码,没有起到他们应有的效果.可能是因为这玩意",    
+    subtiles_24 = "所以那是模因触媒...",    
+    subtiles_25 = "不了解那个。几乎是所有可能发生在我身上的事情，啊，都发生在我身上了.我知道模因触媒是什么感觉",
+    subtiles_26 = "那感觉一点都不像——更像是被从某些东西中解脱了出来，而不是被它强迫着.",    
+    subtiles_27 = "我……我明白了。所以，你真的也不知道发生了什么事?",    
+    subtiles_28 = "不知道.",    
+    subtiles_29 = "操……操.",    
+    subtiles_30 = "所以，你是要赶往某个地方还是四处游荡着自怨自艾",    
+    subtiles_31 = "我要去579那.",    
+    subtiles_32 = "如果你是在自杀，那有着更简单的方法来选，信我!",    
+    subtiles_33 = "你知道那是什么吗?",    
+    subtiles_34 = "毫无头绪——这就很令人担忧了，因为我可是个大人物.",    
+    subtiles_35 = "没关系。我必须得去那里.",   
+    subtiles_36 = "为什么?",    
+    subtiles_37 = "我就是要去。你要去哪里?",    
+    subtiles_38 = "1437。想看看我能不能尿到另一个宇宙去。然后就把这护身符扔进去,看看我会在哪醒来.",    
+    subtiles_39 = "听起来不错。祝你好运.",    
+    subtiles_40 = "我还得祝你好运呢，但是我们俩都知道你是去不到那里的..",    
+    subtiles_41 = "我天快亮了——我要出发了",    
+    subtiles_42 = "好吧.",    
+    subtiles_43 = "我希望你至少能找到你所寻找的东西.",    
+    subtiles_44 = "我也是."
+}
+
+scp_055.AddLanguage("zh-CN", LANG_zh_CN)
